4f3003ea2c173612f0962fe9acaaf9faec21b95e
NikitaVY <105711314+NikitaVY@users.noreply.github.com>
2024-07-04 18:51:42 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,8 @@
+let feedback=[];
+        
+function addFeedback(){
+    let str=document.getElementById("enfb").value;
+    let i=feedback.length;           
+    feedback[i]=str;
+    let j=document.getElementById('fb').value=feedback;
+}
