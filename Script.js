d07225c752cfc7b3009301b58d1630e046a997d8
NikitaVY <105711314+NikitaVY@users.noreply.github.com>
2024-07-10 00:56:47 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,8 +1,7 @@
-let feedback=[];
-        
-function addFeedback(){
-    let str=document.getElementById("enfb").value;
-    let i=feedback.length;           
-    feedback[i]=str;
-    let j=document.getElementById('fb').value=feedback;
-}
+let btn = document.querySelector("button");
+let txt = document.querySelector("#enfb");
+let txt1 = document.querySelector("#fb");
+btn.addEventListener("click",function () {
+    txt1.innerText=txt.value;
+    txt.value="";
+});
